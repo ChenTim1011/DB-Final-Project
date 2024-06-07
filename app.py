f105ea3fcf3ba9c1518de2f601272b0da3bb367b
ryan98153 <ryan98153@gmail.com>
2024-06-07 21:24:57 +0800

    fix: fix the CORP problem between port

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 import sqlite3
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 DATABASE = 'library.db'
 
 def create_tables():
